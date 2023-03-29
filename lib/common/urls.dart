bb4e9f6607d2540351bd48cc972e549fbf4eb31b
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-03-29 22:33:39 +0200

    Update urls.dart

--- lib/common/urls.dart
+++ lib/common/urls.dart
@@ -2,5 +2,5 @@ class AppUrls {
   static String csvFile =
       "https://raw.githubusercontent.com/IsD4n73/BustePaga-Sender/main/src/main/resources/mail.csv";
 
-  static String pdfUserGuide = "";
+  static String pdfUserGuide = "https://github.com/IsD4n73/BP-Sender-2.0/raw/main/assets/resource/BP-sender-userguide.pdf";
 }
