c97931a9e56fd64f6684bd24c81a4cc269e369e0
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-03-29 22:29:52 +0200

    Create user_guide.dart

---
+++ lib/page/user_guide.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class UserGuidePage extends StatefulWidget {
+  const UserGuidePage({Key? key}) : super(key: key);
+
+  @override
+  State<UserGuidePage> createState() => _UserGuidePageState();
+}
+
+class _UserGuidePageState extends State<UserGuidePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: SfPdfViewer.network(),
+    );
+  }
+}
