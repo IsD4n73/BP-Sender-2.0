9196cc56b3fee390d1b87d60858dbd91a9d47f75
dante.davidde <dante.davidde@accenture.com>
2024-03-25 12:49:38 +0100

    resize fix upgrade depend

--- lib/page/file_splitter.dart
+++ lib/page/file_splitter.dart
@@ -52,196 +52,214 @@ class _SplitPageState extends State<SplitPage> {
       );
     }
 
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(MediaQuery.of(context).size.height / 2, 50),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(MediaQuery.of(context).size.height / 2, 50),
+              ),
+              onPressed: () {
+                showGuideDialog(context);
+              },
+              child: const Text(
+                "Guida",
+              ),
             ),
-            onPressed: () {
-              showGuideDialog(context);
-            },
-            child: const Text(
-              "Guida",
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Divider(),
-          const SizedBox(height: 5),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                const Text(
-                  "Divisione PDF",
-                  style: TextStyle(fontSize: 32),
-                ),
-                const SizedBox(height: 45),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        FilePickerResult? result =
-                            await FilePicker.platform.pickFiles(
-                          allowMultiple: false,
-                          type: FileType.custom,
-                          allowedExtensions: ["pdf"],
-                          dialogTitle: "Seleziona il file",
-                        );
+            const SizedBox(height: 20),
+            const Divider(),
+            const SizedBox(height: 5),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Text(
+                    "Divisione PDF",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  const SizedBox(height: 45),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          FilePickerResult? result =
+                              await FilePicker.platform.pickFiles(
+                            allowMultiple: false,
+                            type: FileType.custom,
+                            allowedExtensions: ["pdf"],
+                            dialogTitle: "Seleziona il file",
+                          );
 
-                        if (result != null) {
-                          file = File(result.files.single.path!);
-                          if (CorrectionOcr.correction.isEmpty) {
-                            final response =
-                                await http.get(Uri.parse(AppUrls.ocrFile));
-                            if (response.statusCode == 200) {
-                              CorrectionOcr.fromJson(jsonDecode(response.body));
-                            }
-                          }
-                          try {
-                            await http
-                                .get(Uri.parse('http://127.0.0.1:55004/stop'));
-                          } catch (_) {
-                            debugPrint(
-                                "Il server non è gia spento, shutdown non necessario");
-                          }
-
-                          try {
-                            status = -1;
-                            setState(() {});
-
-                            var instance = ShellManager.getInstance("default");
-                            instance.installRequires(
-                              [
-                                "flask",
-                                "pytesseract",
-                              ],
-                              echo: true,
-                            );
-
-                            instance.runFile("assets/app/main.py", echo: true);
-
-                            while (true) {
-                              try {
-                                var response = await http.get(
-                                  Uri.parse('http://127.0.0.1:55004/'),
-                                );
-                                setState(() {
-                                  if (response.statusCode == 200) {
-                                    debugPrint(response.body.trim());
-                                    status = 0;
-                                  } else {
-                                    status = 1;
-                                  }
-                                });
-                                return;
-                              } catch (e) {
-                                debugPrint("Riprovo a chiamare il servizio");
-                                await Future.delayed(
-                                  const Duration(milliseconds: 500),
-                                );
+                          if (result != null) {
+                            file = File(result.files.single.path!);
+                            if (CorrectionOcr.correction.isEmpty) {
+                              final response =
+                                  await http.get(Uri.parse(AppUrls.ocrFile));
+                              if (response.statusCode == 200) {
+                                CorrectionOcr.fromJson(
+                                    jsonDecode(response.body));
                               }
                             }
-                          } catch (e) {
-                            setState(() {
-                              status = 1;
-                            });
-                          }
+                            try {
+                              await http.get(
+                                  Uri.parse('http://127.0.0.1:55004/stop'));
+                            } catch (_) {
+                              debugPrint(
+                                  "Il server non è gia spento, shutdown non necessario");
+                            }
 
-                          setState(() {});
-                        }
-                      },
-                      child: const Text("Segli File"),
-                    ),
-                    const SizedBox(width: 20),
-                    DropdownButton(
-                      value: type,
-                      items: const [
-                        DropdownMenuItem(
-                          value: "CU",
-                          child: Text("Certificazione Unica"),
-                        ),
-                        DropdownMenuItem(
-                          value: "BP",
-                          enabled: false,
-                          child: Text("Busta Paga - COOMING SOON"),
-                        ),
-                      ],
-                      onChanged: (value) {
-                        setState(() {
-                          type = value ?? "CU";
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                Text(
-                    "File Selezionato: ${file != null ? basename(file!.path) : ""}"),
-                const SizedBox(height: 25),
-                status == 0
-                    ? ElevatedButton(
-                        onPressed: file == null
-                            ? null
-                            : () async {
-                                final cancel = BotToast.showLoading();
+                            try {
+                              status = -1;
+                              setState(() {});
 
+                              var instance =
+                                  ShellManager.getInstance("default");
+                              instance.installRequires(
+                                [
+                                  "flask",
+                                  "pytesseract",
+                                ],
+                                echo: true,
+                              );
+
+                              instance.runFile("assets/app/main.py",
+                                  echo: true);
+
+                              while (true) {
                                 try {
-                                  await createSplittedDir(file!.parent.path);
-                                  await splitFile(file!);
-                                  AlertUtils.showSuccess(
-                                      "La creazione di file separati è andata a buon fine!");
+                                  var response = await http.get(
+                                    Uri.parse('http://127.0.0.1:55004/'),
+                                  );
+                                  setState(() {
+                                    if (response.statusCode == 200) {
+                                      debugPrint(response.body.trim());
+                                      status = 0;
+                                    } else {
+                                      status = 1;
+                                    }
+                                  });
+                                  return;
                                 } catch (e) {
-                                  AlertUtils.showError(
-                                      "Non è possibile dividere questo PDF, riprova.");
-                                } finally {
-                                  cancel();
+                                  debugPrint("Riprovo a chiamare il servizio");
+                                  await Future.delayed(
+                                    const Duration(milliseconds: 500),
+                                  );
                                 }
-                              },
-                        child: const Text("Avvia"),
-                      )
-                    : status == -1
-                        ? const CircularProgressIndicator()
-                        : const SizedBox.shrink(),
-                const SizedBox(height: 25),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: namesController,
-                    maxLines: 10,
-                    readOnly: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'LOG',
+                              }
+                            } catch (e) {
+                              setState(() {
+                                status = 1;
+                              });
+                            }
+
+                            setState(() {});
+                          }
+                        },
+                        child: const Text("Segli File"),
+                      ),
+                      const SizedBox(width: 20),
+                      DropdownButton(
+                        value: type,
+                        items: const [
+                          DropdownMenuItem(
+                            value: "CU",
+                            child: Text("Certificazione Unica"),
+                          ),
+                          DropdownMenuItem(
+                            value: "BP",
+                            enabled: false,
+                            child: Text("Busta Paga - COOMING SOON"),
+                          ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            type = value ?? "CU";
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                      "File Selezionato: ${file != null ? basename(file!.path) : ""}"),
+                  const SizedBox(height: 25),
+                  status == 0
+                      ? ElevatedButton(
+                          onPressed: file == null
+                              ? null
+                              : () async {
+                                  final cancel = BotToast.showLoading();
+
+                                  try {
+                                    await createSplittedDir(file!.parent.path);
+                                    await splitFile(file!);
+                                    AlertUtils.showSuccess(
+                                        "La creazione di file separati è andata a buon fine!");
+                                  } catch (e) {
+                                    AlertUtils.showError(
+                                        "Non è possibile dividere questo PDF, riprova.");
+                                  } finally {
+                                    cancel();
+                                  }
+                                },
+                          child: const Text("Avvia"),
+                        )
+                      : status == -1
+                          ? const CircularProgressIndicator()
+                          : const SizedBox.shrink(),
+                  const SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: namesController,
+                      maxLines: 10,
+                      readOnly: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'LOG',
+                      ),
                     ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(
-                    onPressed: namesController.text.isEmpty
-                        ? null
-                        : () async {
-                            DateTime currentDate = DateTime.now();
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: namesController.text.isEmpty
+                          ? null
+                          : () async {
+                              DateTime currentDate = DateTime.now();
 
-                            var settings = await getSavedSettings();
-                            if (!settings.takeExeDir) {
-                              dir = await FilePicker.platform.getDirectoryPath(
-                                    dialogTitle:
-                                        "Seleziona la cartella dove salvare il file",
-                                  ) ??
-                                  "NULL";
-                              if (dir != "NULL") {
+                              var settings = await getSavedSettings();
+                              if (!settings.takeExeDir) {
+                                dir =
+                                    await FilePicker.platform.getDirectoryPath(
+                                          dialogTitle:
+                                              "Seleziona la cartella dove salvare il file",
+                                        ) ??
+                                        "NULL";
+                                if (dir != "NULL") {
+                                  try {
+                                    String completeLog = namesController.text;
+
+                                    final File file = File(
+                                        '$dir/log-${currentDate.day}-${currentDate.month}-${currentDate.year}-${currentDate.second}.txt');
+                                    await file.writeAsString(completeLog);
+                                    AlertUtils.showSuccess(
+                                        "Il file è stato salvato in $dir");
+                                  } catch (_) {
+                                    AlertUtils.showError(
+                                        "Non è stato possibile salvare il file");
+                                  }
+                                }
+                              } else {
                                 try {
-                                  String completeLog = namesController.text;
-
                                   final File file = File(
                                       '$dir/log-${currentDate.day}-${currentDate.month}-${currentDate.year}-${currentDate.second}.txt');
-                                  await file.writeAsString(completeLog);
+                                  await file
+                                      .writeAsString(namesController.text);
                                   AlertUtils.showSuccess(
                                       "Il file è stato salvato in $dir");
                                 } catch (_) {
@@ -249,26 +267,15 @@ class _SplitPageState extends State<SplitPage> {
                                       "Non è stato possibile salvare il file");
                                 }
                               }
-                            } else {
-                              try {
-                                final File file = File(
-                                    '$dir/log-${currentDate.day}-${currentDate.month}-${currentDate.year}-${currentDate.second}.txt');
-                                await file.writeAsString(namesController.text);
-                                AlertUtils.showSuccess(
-                                    "Il file è stato salvato in $dir");
-                              } catch (_) {
-                                AlertUtils.showError(
-                                    "Non è stato possibile salvare il file");
-                              }
-                            }
-                          },
-                    child: const Text("Salva Log"),
+                            },
+                      child: const Text("Salva Log"),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
