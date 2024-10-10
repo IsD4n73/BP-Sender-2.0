133df88ccac76a5514234a37ea3d7e6639ff08fc
DΛПƬΣ <123465997+dante-accenture@users.noreply.github.com>
2024-10-10 10:35:00 +0200

    Update

--- lib/common/base_credential.dart
+++ lib/common/base_credential.dart
@@ -2,8 +2,8 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 class BaseCredential {
   static String username = "smtp@3em.it";
-  static String password = "qCcHVn4WbgVwckW@";
-  static String email = "f.scarmozzino@3em.it";
+  static String password = "";
+  static String email = "";
 
   static String prefix = "Busta Paga:";
 
